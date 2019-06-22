17bfddccba9ec7e1598407ef7ac4230a6cb9d926
Anand Vijay Rajsri <avrajsri@gmail.com>
2019-06-22 13:09:24 +0530

    Update pro code.py

--- pro code.py
+++ pro code.py
@@ -113,15 +113,17 @@ def TrainImages():
 #####################################################################################
 
 def TrackImages():
-    recognizer = cv2.face.LBPHFaceRecognizer_create()  # cv2.createLBPHFaceRecognizer()
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
     recognizer.read("TrainingImageLabel\Trainner.yml")
     harcascadePath = "haarcascade_frontalface_default.xml"
     faceCascade = cv2.CascadeClassifier(harcascadePath);
-    df = pd.read_csv("StudentDetails\StudentDetails.csv")
-    cam = cv2.VideoCapture(0)
     font = cv2.FONT_HERSHEY_SIMPLEX
+    cam = cv2.VideoCapture(0)
+    #######################################################
+    df = pd.read_csv("StudentDetails\StudentDetails.csv")
     col_names = ['Id', 'Name', 'Date', 'Time']
     attendance = pd.DataFrame(columns=col_names)
+    #######################################################
     while True:
         ret, im = cam.read()
         gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
@@ -129,6 +131,7 @@ def TrackImages():
         for (x, y, w, h) in faces:
             cv2.rectangle(im, (x, y), (x + w, y + h), (225, 0, 0), 2)
             Id, conf = recognizer.predict(gray[y:y + h, x:x + w])
+
             if (conf < 50):
                 ts = time.time()
                 date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
@@ -143,22 +146,26 @@ def TrackImages():
             if (conf > 75):
                 noOfFile = len(os.listdir("ImagesUnknown")) + 1
                 cv2.imwrite("ImagesUnknown\Image" + str(noOfFile) + ".jpg", im[y:y + h, x:x + w])
-            cv2.putText(im, str(tt), (x, y + h), font, 1, (255, 255, 255), 2)
+
+            cv2.putText(im, str(tt), (x, y + h), font, 1, (255, 255, 255), 3)
         attendance = attendance.drop_duplicates(subset=['Id'], keep='first')
+
         cv2.imshow('im', im)
-        if (cv2.waitKey(1) == ord('q')):
+        if cv2.waitKey(10) & 0xFF == ord('q'):
             break
+
+    ##########################################################
     ts = time.time()
     date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
     timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
     Hour, Minute, Second = timeStamp.split(":")
     fileName = "Attendance\Attendance_" + date + "_" + Hour + "-" + Minute + "-" + Second + ".csv"
     attendance.to_csv(fileName, index=False)
+    ################################################
+
     cam.release()
     cv2.destroyAllWindows()
-    # print(attendance)
-    res = attendance
-    message2.configure(text=res)
+    message2.configure(text=attendance)
 
 #####################################################################################
 
