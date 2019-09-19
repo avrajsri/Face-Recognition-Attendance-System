ef4456e267177f6ac25627f3e2b9625ab3545913
Anand Vijay Rajsri <avrajsri@gmail.com>
2019-09-19 20:04:24 +0530

    Update pro code.py

--- pro code.py
+++ pro code.py
@@ -1,16 +1,32 @@
-import os, cv2, csv, datetime
+import os
+import cv2
+import csv
+import datetime
+import pyttsx3
 import numpy as np
 import pandas as pd
 from tkinter import *
 from time import *
 from PIL import Image
 
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def talkToMe(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+talkToMe("Welcome to Recognising System")
 root = Tk()
 root.geometry('690x550')
-root.title("Face Recognition Attendance System")
+root.title("Automatic Recognising Attendance System")
 root.resizable(width=FALSE, height=FALSE)
 
 root.configure(background='#FFFFFF')
+
 
 def center(win):
     win.update_idletasks()
@@ -20,10 +36,12 @@ def center(win):
     y = (win.winfo_screenheight() // 2) - (height // 2)
     win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
 
+
 def assure_path_exists(path):
     dir = os.path.dirname(path)
     if not os.path.exists(dir):
         os.makedirs(dir)
+
 
 center(root)
 assure_path_exists("Attendance/")
@@ -31,33 +49,36 @@ assure_path_exists("TakeImages/")
 assure_path_exists("TrainingImage/")
 assure_path_exists("UnknownUser/")
 
+
 def tick(time1=''):
     time2 = strftime('%H:%M:%S')
     if time2 != time1:
         l1.config(text=time2)
     l1.after(200, tick)
 
+
 l1 = Label(root, font=('arial 15 bold'), background='#FFFFFF')
-l1.place(x=370,y=10)
+l1.place(x=370, y=10)
 y = strftime("%d/%m/%y")
-Label(root, text=y, font=('arial 16 bold'), background='#FFFFFF').place(x=250,y=10)
+Label(root, text=y, font=('arial 16 bold'), background='#FFFFFF').place(x=250, y=10)
 tick()
 
 lbl = Label(root, text="Enter ID: ", width=20, fg="#0055ff", bg="white", font=('times', 17, ' bold '))
 lbl.place(x=110, y=80)
-txt = Entry(root, width=20, bg="white", fg="red",bd=4, font=('times', 15, ' bold '))
+txt = Entry(root, width=20, bg="white", fg="red", bd=4, font=('times', 15, ' bold '))
 txt.focus_set()
 txt.place(x=340, y=80)
 
 lbl2 = Label(root, text="Enter Name: ", width=20, fg="#0055ff", bg="white", font=('times', 17, ' bold '))
 lbl2.place(x=100, y=150)
-txt2 = Entry(root, width=20, bg="white", fg="red",bd=4, font=('times', 15, ' bold '))
+txt2 = Entry(root, width=20, bg="white", fg="red", bd=4, font=('times', 15, ' bold '))
 txt2.place(x=340, y=150)
 
 lbl3 = Label(root, text="Notification: ", width=10, fg="red", bg="white", height=2, font=('times', 17, ' bold'))
 lbl3.place(x=50, y=340)
-message = Label(root, text="", fg="#0055ff", bg="white", bd=4, relief=GROOVE, activebackground="yellow", width=35, height=2, font=('times', 15, ' bold '))
-message.configure(text="Welcome Face Recognition Attendance System")
+message = Label(root, text="", fg="#0055ff", bg="white", bd=4, relief=GROOVE, activebackground="yellow", width=35,
+                height=2, font=('times', 15, ' bold '))
+message.configure(text="Welcome to Recognising System")
 message.place(x=210, y=340)
 
 lbl4 = Label(root, text="Attendance: ", width=10, fg="red", bg="white", height=2, font=('times', 17, ' bold'))
@@ -65,37 +86,55 @@ lbl4.place(x=50, y=445)
 
 frame = Frame(root)
 frame.place(x=210, y=420)
-listNodes = Listbox(frame,  width=42, height=4, bd=4, font=('times', 15, ' bold '), selectbackground="blue", activestyle="none")
+listNodes = Listbox(frame, width=42, height=4, bd=4, font=('times', 15, ' bold '), selectbackground="blue",
+                    activestyle="none")
 listNodes.pack(side="left", fill="y")
 scrollbar = Scrollbar(frame, orient="vertical")
 scrollbar.config(command=listNodes.yview)
 scrollbar.pack(side="right", fill="y")
 listNodes.config(yscrollcommand=scrollbar.set)
 
-show="   'Date'            'Time'             'Id'           'Name'                   "
+show = "   'Date'            'Time'             'Id'           'Name'                   "
 lbl5 = Label(root, text=show, width=41, fg="#0055ff", bg="white", height=0, font=('times', 13, ' bold'))
 lbl5.place(x=230, y=425)
 
+
 def is_number(s):
+    name = (txt2.get())
     try:
         float(s)
-        return True
-    except ValueError:
-        pass
-    try:
-        import unicodedata
-        unicodedata.numeric(s)
+        name.isalpha()
         return True
     except (TypeError, ValueError):
-        pass
+        res = "Enter Only Valid Data"
+        message.configure(text=res)
     return False
+
 
 def TakeImages():
     Id = (txt.get())
     name = (txt2.get())
 
-    if(len(Id)==0 & len(name)==0):
-        res = "Enter Numeric Id"
+    if (len(Id) == 0 & len(name) == 0):
+        res = "First Enter Numeric Id"
+        message.configure(text=res)
+        talkToMe(res)
+
+    elif (len(Id) == 0):
+        res = "First Enter Numeric Id"
+        message.configure(text=res)
+        talkToMe(res)
+
+    elif (len(name) == 0):
+        res = "And Enter Alphabetical Name"
+        message.configure(text=res)
+        talkToMe(res)
+
+    try:
+        float(Id)
+        name.isalpha()
+    except (TypeError, ValueError):
+        res = "Enter Only Valid Data"
         message.configure(text=res)
 
     if (is_number(Id) and name.isalpha()):
@@ -108,31 +147,31 @@ def TakeImages():
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             faces = detector.detectMultiScale(gray, 1.3, 5)
             for (x, y, w, h) in faces:
-                cv2.rectangle(img, (x-20,y-20), (x+w+20,y+h+20), (0,255,0), 4)
+                cv2.rectangle(img, (x - 20, y - 20), (x + w + 20, y + h + 20), (0, 255, 0), 4)
                 sampleNum = sampleNum + 1
                 cv2.imwrite("TakeImages/ " + name + "." + Id + '.' + str(sampleNum) + ".jpg", gray[y:y + h, x:x + w])
                 cv2.imshow('frame', img)
             if cv2.waitKey(25) & 0xFF == ord('a'):
                 break
-            elif sampleNum > 29:
+            elif sampleNum > 59:
                 break
 
+        txt.delete(0, END)
+        txt2.delete(0, END)
+        txt.focus_set()
         cam.release()
         cv2.destroyAllWindows()
-        res = "Images Saved for ID : " + Id + " Name : " + name
+        res = "Images Saved for ID: " + Id + ", Name: " + name
         row = [Id, name]
         with open('Details/Details.csv', 'a+') as csvFile:
             writer = csv.writer(csvFile)
             writer.writerow(row)
         csvFile.close()
         message.configure(text=res)
+        talkToMe(res)
     else:
-        if (is_number(Id)):
-            res = "Enter Alphabetical Name"
-            message.configure(text=res)
-        if (name.isalpha()):
-            res = "Enter Numeric Id"
-            message.configure(text=res)
+        res = "Enter Only Valid Data"
+        talkToMe(res)
 
 
 def TrainImages():
@@ -158,13 +197,16 @@ def TrainImages():
         recognizer.train(faces, np.array(Id))
         recognizer.save("TrainingImage/Trainner.yml")
         message.configure(text="Image Trained")
+        talkToMe("Image Trained")
 
     except Exception as e:
         message.configure(text="First Add Students")
+        talkToMe("First Add Students")
+
 
 def TrackImages():
     try:
-        listNodes.delete(0,END)
+        listNodes.delete(0, END)
         recognizer = cv2.face.LBPHFaceRecognizer_create()
         recognizer.read("TrainingImage/Trainner.yml")
         harcascadePath = "haarcascade_frontalface_default.xml"
@@ -181,16 +223,16 @@ def TrackImages():
             gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
             faces = faceCascade.detectMultiScale(gray, 1.2, 5)
             for (x, y, w, h) in faces:
-                cv2.rectangle(im, (x-20,y-20), (x+w+20,y+h+20), (0,255,0), 4)
-                cv2.rectangle(im, (x-22,y-90), (x+w+22, y-22), (0,255,0), -1)
-                Id, conf = recognizer.predict(gray[y:y+h,x:x+w])
+                cv2.rectangle(im, (x - 20, y - 20), (x + w + 20, y + h + 20), (0, 255, 0), 4)
+                cv2.rectangle(im, (x - 22, y - 90), (x + w + 22, y - 22), (0, 255, 0), -1)
+                Id, conf = recognizer.predict(gray[y:y + h, x:x + w])
 
                 if (conf < 50):
                     ts = time()
                     date = datetime.datetime.fromtimestamp(ts).strftime('%d/%m/%y')
                     timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
                     name = df.loc[df['Id'] == Id]['Name'].values
-                    name=(name[0])
+                    name = (name[0])
                     mess = str(Id) + " " + name
                     attendance.loc[len(attendance)] = [Id, name, date, timeStamp]
 
@@ -219,31 +261,41 @@ def TrackImages():
 
         cam.release()
         cv2.destroyAllWindows()
-        count= 0
+        count = 0
 
         with open(fileName, 'r')as r:
             data = csv.reader(r)
-            z1="   "
-            z2="     "
-            z3="     "
-            z4="          "
+            z1 = "   "
+            z2 = "     "
+            z3 = "     "
+            z4 = "          "
             for row in data:
-                count+=1
+                count += 1
                 listNodes.insert(END, z1 + row[2] + z2 + row[3] + z3 + row[0] + z4 + row[1])
+                if (count == 1):
+                    talkToMe("Wlecome ")
+                if (count > 1):
+                    talkToMe(row[1])
 
-        count-=1
-        message.configure(text=str(count)+" Students Attendance Record")
-
+        count -= 1
+        message.configure(text=str(count) + " Students Attendance Record")
+        talkToMe(str(count) + " Students Attendance Record")
 
     except Exception as e:
         message.configure(text="First Train Images")
+        talkToMe("First Train Images")
 
-takeImg = Button(root, text="Add Student", command=TakeImages, bg="#3eff00", activebackground="#0055ff", activeforeground="white", bd=4, font=('times', 15, ' bold '))
+
+takeImg = Button(root, text="Add Student", command=TakeImages, bg="#3eff00", activebackground="#0055ff",
+                 activeforeground="white", bd=4, font=('times', 15, ' bold '))
 takeImg.place(x=50, y=240)
-trainImg = Button(root, text="Train Images", command=TrainImages, bg="#3eff00", activebackground="#0055ff", activeforeground="white", bd=4, font=('times', 15, ' bold '))
+trainImg = Button(root, text="Train Images", command=TrainImages, bg="#3eff00", activebackground="#0055ff",
+                  activeforeground="white", bd=4, font=('times', 15, ' bold '))
 trainImg.place(x=220, y=240)
-trackImg = Button(root, text="Track Images", command=TrackImages, bg="#3eff00", activebackground="#0055ff", activeforeground="white", bd=4, font=('times', 15, ' bold '))
+trackImg = Button(root, text="Track Images", command=TrackImages, bg="#3eff00", activebackground="#0055ff",
+                  activeforeground="white", bd=4, font=('times', 15, ' bold '))
 trackImg.place(x=390, y=240)
-quitroot = Button(root, text="Quit",fg="white", command=root.destroy, bg="#ff1111", activebackground="Red", activeforeground="white", bd=4, font=('times', 15, ' bold '))
+quitroot = Button(root, text="Quit", fg="white", command=root.destroy, bg="#ff1111", activebackground="Red",
+                  activeforeground="white", bd=4, font=('times', 15, ' bold '))
 quitroot.place(x=570, y=240)
 root.mainloop()
