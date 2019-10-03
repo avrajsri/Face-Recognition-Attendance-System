43018c848f370189a2210f7b55ba737dd40a99f0
Anand Vijay Rajsri <avrajsri@gmail.com>
2019-10-03 19:41:40 +0530

    Update pro code.py

--- pro code.py
+++ pro code.py
@@ -13,7 +13,6 @@ engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
-
 def talkToMe(audio):
     engine.say(audio)
     engine.runAndWait()
